aa76e2e3da2154392fd459b338875c4e45b7d867
Clint Jude Chavez <clintjudechavez@Clints-MacBook-Air.local>
2026-01-14 18:27:56 +0800

    Initial commit

---
+++ src/tester.java
@@ -0,0 +1,10 @@
+public class tester {
+    public static void main(String[] args) {
+
+        SimpleFrame frame = new SimpleFrame();
+        frame.showIt();
+        frame.showIt("Computer Programming 2. First Assignment.", 100, 100);
+
+
+    }
+}
